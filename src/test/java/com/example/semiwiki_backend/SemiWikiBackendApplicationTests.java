53d19e70bcbaaf4084463e12e867633fd8f443e1
hansaem <ihansaem60@gmail.com>
2025-07-28 21:07:25 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/example/semiwiki_backend/SemiWikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semiwiki_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiWikiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
