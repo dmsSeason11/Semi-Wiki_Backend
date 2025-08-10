5b86beda4c530450f61ca7279bb70efae2591d3a
treegus <cowtree28@gmail.com>
2025-08-10 22:07:37 +0900

    feat: (#8) 엔티티 repository 수정
    repository의 @repository 추가

--- src/main/java/com/example/semiwiki_backend/domain/notice_table/repository/NoticeTableRepository.java
+++ src/main/java/com/example/semiwiki_backend/domain/notice_table/repository/NoticeTableRepository.java
@@ -2,6 +2,8 @@ package com.example.semiwiki_backend.domain.notice_table.repository;
 
 import com.example.semiwiki_backend.domain.notice_table.entity.NoticeTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoticeTableRepository extends JpaRepository<NoticeTable, Integer> {
 }
