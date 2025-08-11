3720efff59f2fac891089e4e14d05f384e385301
hansaem <ihansaem60@gmail.com>
2025-08-11 22:28:27 +0900

    feat: (#4) 로그인 회원가입 구현완료

--- src/main/java/com/example/semiwiki_backend/domain/auth/service/AuthService.java
+++ src/main/java/com/example/semiwiki_backend/domain/auth/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.example.semiwiki_backend.domain.auth.service;
 
+import ch.qos.logback.classic.spi.IThrowableProxy;
 import com.example.semiwiki_backend.domain.auth.dto.SignInRequest;
 import com.example.semiwiki_backend.domain.auth.dto.SignUpRequest;
 import com.example.semiwiki_backend.domain.auth.dto.TokenResponse;
+import com.example.semiwiki_backend.domain.auth.exception.AccountAlreadyExistsException;
+import com.example.semiwiki_backend.domain.auth.exception.AccountNotFoundException;
+import com.example.semiwiki_backend.domain.auth.exception.IncorrectPasswordException;
 import com.example.semiwiki_backend.domain.user.entity.User;
 import com.example.semiwiki_backend.domain.user.repository.UserRepository;
 import com.example.semiwiki_backend.global.security.auth.Role;
@@ -23,7 +27,7 @@ public class AuthService {
     @Transactional
     public TokenResponse join(SignUpRequest signUpRequest) {
         if(userRepository.existsByAccountId(signUpRequest.getAccountId())) {
-            throw new IllegalArgumentException ("Account already exists");
+            throw AccountAlreadyExistsException.EXCEPTION;
         }
 
         User user=User.builder()
@@ -41,10 +45,10 @@ public class AuthService {
 
     public TokenResponse login(SignInRequest signInRequest) {
         User user =userRepository.findByAccountId(signInRequest.getAccountId())
-            .orElseThrow(()->new IllegalArgumentException("Account not found"));
+            .orElseThrow(()-> AccountNotFoundException.EXCEPTION);
 
         if(!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
-            throw new IllegalArgumentException ("Incorrect password");
+            throw IncorrectPasswordException.EXCEPTION;
         }
 
         return new TokenResponse(jwtTokenProvider.generateAccessToken(user.getAccountId()));
--- src/main/java/com/example/semiwiki_backend/domain/notice_table/repository/NoticeTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.semiwiki_backend.domain.notice_table.repository;
-
-import com.example.semiwiki_backend.domain.notice_table.entity.NoticeTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticeTableRepository extends JpaRepository<NoticeTable, Integer> {
-}
