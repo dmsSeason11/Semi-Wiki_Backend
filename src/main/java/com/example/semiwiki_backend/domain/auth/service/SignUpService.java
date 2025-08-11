3a381249230bf8d941b180d4b7adfb4a7749f5b1
hansaem <ihansaem60@gmail.com>
2025-08-11 23:32:50 +0900

    feat: (#4) 로그아웃 기능 추가

--- src/main/java/com/example/semiwiki_backend/domain/auth/service/SignUpService.java
+++ src/main/java/com/example/semiwiki_backend/domain/auth/service/SignUpService.java
@@ -1,12 +1,8 @@
 package com.example.semiwiki_backend.domain.auth.service;
 
-import ch.qos.logback.classic.spi.IThrowableProxy;
-import com.example.semiwiki_backend.domain.auth.dto.SignInRequest;
 import com.example.semiwiki_backend.domain.auth.dto.SignUpRequest;
 import com.example.semiwiki_backend.domain.auth.dto.TokenResponse;
 import com.example.semiwiki_backend.domain.auth.exception.AccountAlreadyExistsException;
-import com.example.semiwiki_backend.domain.auth.exception.AccountNotFoundException;
-import com.example.semiwiki_backend.domain.auth.exception.IncorrectPasswordException;
 import com.example.semiwiki_backend.domain.user.entity.User;
 import com.example.semiwiki_backend.domain.user.repository.UserRepository;
 import com.example.semiwiki_backend.global.security.auth.Role;
@@ -16,42 +12,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @RequiredArgsConstructor
-public class AuthService {
+public class SignUpService {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public TokenResponse join(SignUpRequest signUpRequest) {
+    public TokenResponse execute(SignUpRequest signUpRequest) {
         if(userRepository.existsByAccountId(signUpRequest.getAccountId())) {
             throw AccountAlreadyExistsException.EXCEPTION;
         }
 
         User user=User.builder()
-            .accountId(signUpRequest.getAccountId())
-            .studentNum(signUpRequest.getStudentNum())
-            .username(signUpRequest.getUsername())
-            .password(passwordEncoder.encode(signUpRequest.getPassword()))
-            .role(Role.ROLE_USER)
-            .build();
+                .accountId(signUpRequest.getAccountId())
+                .studentNum(signUpRequest.getStudentNum())
+                .username(signUpRequest.getUsername())
+                .password(passwordEncoder.encode(signUpRequest.getPassword()))
+                .role(Role.ROLE_USER)
+                .build();
 
         userRepository.save(user);
 
         return new TokenResponse(jwtTokenProvider.generateAccessToken(user.getAccountId()));
     }
-
-    public TokenResponse login(SignInRequest signInRequest) {
-        User user =userRepository.findByAccountId(signInRequest.getAccountId())
-            .orElseThrow(()-> AccountNotFoundException.EXCEPTION);
-
-        if(!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
-            throw IncorrectPasswordException.EXCEPTION;
-        }
-
-        return new TokenResponse(jwtTokenProvider.generateAccessToken(user.getAccountId()));
-    }
-
 }
