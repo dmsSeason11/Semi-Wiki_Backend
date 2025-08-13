1c6f02fd586653857b60b4c0457485a692c1a3e9
treegus <cowtree28@gmail.com>
2025-08-13 15:45:59 +0900

    feat: (#5) 페이지네이션 기능 추가

--- src/main/java/com/example/semiwiki_backend/domain/notice_board/repository/NoticeBoardRepository.java
+++ src/main/java/com/example/semiwiki_backend/domain/notice_board/repository/NoticeBoardRepository.java
@@ -1,6 +1,8 @@
 package com.example.semiwiki_backend.domain.notice_board.repository;
 
 import com.example.semiwiki_backend.domain.notice_board.entity.NoticeBoard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,11 +11,14 @@ import java.util.List;
 
 public interface NoticeBoardRepository extends JpaRepository<NoticeBoard, Integer> {
     @Query("SELECT n FROM NoticeBoard n JOIN n.categories c WHERE c IN :categories GROUP BY n HAVING COUNT(c) = SIZE(n.categories) ")
-    List<NoticeBoard> findByCategoriesAllMatch(@Param("categories") List<String> categories);
+    List<NoticeBoard> findByCategoriesAllMatch(@Param("categories") List<String> categories, Pageable pageable);
 
 
-    List<NoticeBoard> findByTitleContainingIgnoreCase(String title);
+    List<NoticeBoard> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     @Query(" SELECT n FROM NoticeBoard n JOIN n.categories c WHERE LOWER(n.title) LIKE LOWER(CONCAT('%', :title, '%')) AND c IN :categories GROUP BY n HAVING COUNT(c) = SIZE(n.categories) ")
-    List<NoticeBoard> findAllByTitleContainingAndCategories(String title, List<String> categories);
+    List<NoticeBoard> findAllByTitleContainingAndCategories(String title, List<String> categories, Pageable pageable);
+
+    @Query("SELECT n FROM NoticeBoard n")
+    List<NoticeBoard> findAllNoticeBoards(Pageable pageable);
 }
