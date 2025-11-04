e7db560f7e62f8e7d5ba8319c5fd5be5c3226dd4
treegus <cowtree28@gmail.com>
2025-11-04 20:26:28 +0900

    feat: (#91) JpaRepository에 @Repository 추가

--- src/main/java/com/example/semiwiki_backend/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/semiwiki_backend/domain/comment/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.example.semiwiki_backend.domain.comment.repository;
 
 import com.example.semiwiki_backend.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
