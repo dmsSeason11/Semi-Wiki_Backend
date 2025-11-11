177c30488d83f6c664a612b0ee89cda64fabeffb
yellowstarr0323 <ihansaem60@gmail.com>
2025-11-11 22:49:10 +0900

    refactor: (#106) RedisConfig파일 호스트와 포트 값 넣는 로직

--- src/main/java/com/example/semiwiki_backend/global/config/RedisConfig.java
+++ src/main/java/com/example/semiwiki_backend/global/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.example.semiwiki_backend.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -11,11 +13,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        if (redisPassword != null && !redisPassword.isEmpty()) {
+            redisStandaloneConfiguration.setPassword(redisPassword);
+        }
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
+
 
     @Bean
     public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
